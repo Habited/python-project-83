2181788405d516bbeab97d488c3c3a2ad37746be
Habited <pdskate88@gmail.com>
2025-10-18 22:55:24 +0300

    aded file: start.sh and check file: build.sh - for the render

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+set -e
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-make install && psql -a -d $DATABASE_URL -f database.sql
+source "$HOME/.local/bin/env"
+make install 
