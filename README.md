60c1ac0929b3c36157d9a1e5d4dc7726a3e3ffca
Habited <pdskate88@gmail.com>
2025-10-26 18:07:59 +0300

    rename files: page_analyzer/tools.py -> page_analyzer/database.py, age_analyzer/util.py -> page_analyzer/url_normalyzer.py; added file page_analyzer/parser.py - the process of parsing table headers has been moved to the parser; chenged files: README.md - the file has been finalized, page_analyzer/app.py - the file has been finalized

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-### Hexlet tests and linter status:
-[![Actions Status](https://github.com/Habited/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Habited/python-project-83/actions)
+# Проект page_analyzer 
 
-## Ссылка на сайт "Анализатор страниц" https://python-project-83-6ly6.onrender.com
+Проект page_analyzer разработан для анализа urls адресов.
+
+[![Actions Status](https://github.com/Habited/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Habited/python-project-83/actions)
+[![SonarCloud](https://sonarcloud.io/images/project_badges/sonarcloud-orange.svg)](https://sonarcloud.io/summary/new_code?id=python-project-83)
+
+## Содержание
+
+- [Основные возможности](#-основные-возможности)
+- [Технологии](#-технологии)
+- [Установка](#-установка)
+- [Конфигурация](#-конфигурация)
+- [Лицензия](#-лицензия)
+- [Ссылка на сайт "Анализатор страниц"](#-Ссылка на сайт "Анализатор страниц")
+- [Контакты](#-контакты)
+
+## Основные возможности
+
+Позволяет добавлять url адрес в список адресов, а так же проверять url адреса на (status code, h1, created_at, title, discription)
+Проект page_analyzer разработан для анализа urls адресов.
+
+## Технологии
+
+- **Язык**: Python 3.12
+- **Фреймворки**: Flask
+- **Инструменты**: PostgreSQL, urllib3, ruff, python-dotenv, validators, bs4, psycorg2, requests, uv
+
+## Установка
+
+- git clone https://github.com/Habited/python-project-83.git
+- uv sync
+- make start
+
+## Конфигурация
+- DATABASE_URL=postgresql://user:pass@localhost/mydb
+- DEBUG=true
+
+## Лицензия
+Этот проект распространяется под лицензией MIT — см. файл LICENSE .
+
+## Ссылка на сайт "Анализатор страниц"
+https://python-project-83-6ly6.onrender.com
+
+## Контакты
+Автор: Habited
+Github: @Habited
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -15,8 +15,9 @@ from flask import (
     url_for,
 )
 
-from .tools import DataBase
-from .util import normalize_url
+from .database import DataBase
+from .parser import parser
+from .url_normalyzer import normalize_url
 
 load_dotenv()
 
@@ -29,8 +30,10 @@ db = DataBase(os.getenv("DATABASE_URL"))
 @app.route("/")
 def index():
     messages = get_flashed_messages()
-    return render_template("index.html",
-                           messages=messages)
+    return render_template(
+        "index.html",
+        messages=messages
+    )
 
 
 @app.route("/urls", methods=["GET", "POST"])
@@ -39,29 +42,43 @@ def show_urls():
     if request.method == "GET":
         urls = db.get_all_urls()
         app.logger.info(urls)
-        return render_template("urls/urls.html",
-                               urls=urls,
-                               messages=messages)
+        return render_template(
+            "urls/urls.html",
+            urls=urls,
+            messages=messages
+        )
     
     if request.method == "POST":
         url = request.form.get("url")
         if not url:
-            return render_template("index.html",
-                                   error="URL не может быть пустым"), 422
+            return render_template(
+                "index.html",
+                error="URL не может быть пустым"
+            ), 422
 
         if not validators.url(url):
-            return render_template("index.html", error="Некорректный URL"), 422
+            return render_template(
+                "index.html",
+                error="Некорректный URL"
+            ), 422
         
-        normalized = normalize_url(url)
-        
-        if db.url_exists(normalized):
-            existing_id = db.get_url_id_by_name(normalized)
+        if db.url_exists(normalize_url(url)):
+            existing_id = db.get_url_id_by_name(normalize_url(url))
             flash("Страница уже существует")
-            return redirect(url_for("show_url", url_id=existing_id))
+            return redirect(
+                url_for("show_url",
+                        url_id=existing_id
+                )
+            )
                 
-        url_id = db.add_new_url(normalized, date.today())
+        url_id = db.add_new_url(normalize_url(url), date.today())
         flash("Страница успешно добавлена")
-        return redirect(url_for("show_url", url_id=url_id))
+        return redirect(
+            url_for(
+                "show_url",
+                url_id=url_id
+            )
+        )
 
 
 @app.route("/urls/<int:url_id>")
@@ -72,32 +89,23 @@ def show_url(url_id):
         return render_template("urls/errors.html")
     
     checks = db.get_checks_by_url_id(url_id)
-    return render_template("urls/show.html",
-                           url=url,
-                           url_checks=checks,
-                           messages=messages)
+    return render_template(
+        "urls/show.html",
+        url=url,
+        url_checks=checks,
+        messages=messages
+    )
 
 
 @app.route("/urls/<int:url_id>/checks", methods=["POST"])
 def new_verification_url(url_id):
     url = db.get_url_by_id(url_id)
+    status_code, h1, title, description = parser(url)
     if not url:
         flash("URL не найден")
         return redirect(url_for("show_urls"))
 
     try:
-        response = requests.get(url["name"], timeout=5)
-        response.raise_for_status()
-        status_code = response.status_code
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-        h1 = soup.h1.get_text().strip() if soup.h1 else ''
-        title = soup.title.string.strip() if soup.title else ''
-        
-        description_tag = soup.find('meta', attrs={'name': 'description'})
-        description = description_tag.get(
-            'content', '').strip() if description_tag else ''
-
         db.add_check(url_id, status_code, h1, title, description, date.today())
         flash("Страница успешно проверена")
     except Exception as e:
