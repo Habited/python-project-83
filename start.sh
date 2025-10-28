a5b417bb014bbe37546e73cba45741f48df19f9c
Habited <113029446+Habited@users.noreply.github.com>
2025-10-28 20:02:56 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-apt-get update && apt-get install -y postgresql-client
-
-psql "$DATABASE_URL" -f database.sql
-
-exec gunicorn -w 5 -b 0.0.0.0:${PORT:-10000} page_analyzer:app
